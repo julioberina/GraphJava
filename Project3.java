fab4f0c0c2c0752f4889d842ba14c3d52f1dbe29
Julio Berina <julioberina@gmail.com>
2018-02-28 21:40:31 -0800

    Setup menu loop prototype for entering commands

--- Project3.java
+++ Project3.java
@@ -1,7 +1,47 @@
+import java.util.Scanner;
+
 public class Project3
 {
+    private Scanner scan;
+
     public static void main(String[] args)
     {
-        
+        Digraph dgraph = new Digraph("city.dat", "road.dat");
+        boolean done = false;
+        scan = new Scanner(System.in);
+
+        while (done == false)
+        {
+            done = menuCommandLoop();
+            System.out.print("\n");
+        }
+    }
+
+    private static boolean menuCommandLoop()
+    {
+        String command = "";
+        String cityCode = "";
+        int distance = 0;
+
+        System.out.print("Command?  ");
+        command = scan.nextLine();
+
+        if (command.equalsIgnoreCase("Q"))
+            return false;
+        else if (command.equalsIgnoreCase("D"))
+            return false;
+        else if (command.equalsIgnoreCase("I"))
+            return false;
+        else if (command.equalsIgnoreCase("R"))
+            return false;
+        else if (command.equalsIgnoreCase("H"))
+            return false;
+        else if (command.equalsIgnoreCase("E"))
+            return true;
+        else
+        {
+            System.out.println("Invalid input!");
+            return false;
+        }
     }
 }
