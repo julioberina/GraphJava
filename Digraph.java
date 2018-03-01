616e31bab0c67486c022cef12ee525ec31c1f097
Julio Berina <julioberina@gmail.com>
2018-02-28 21:25:35 -0800

    Setup initial fields for Digraph class

--- Digraph.java
+++ Digraph.java
@@ -1,4 +1,9 @@
+import java.util.HashMap;
+
 public class Digraph
 {
-    
+    // Necessary fields for graph
+    private HashMap<String, Integer> cityIndex;
+    private int[][] graph; // size 21 (0-20, 0 blank), graph[source][target]
+    private String[] cityData; // size 21 (0-20, 0 blank)
 }
